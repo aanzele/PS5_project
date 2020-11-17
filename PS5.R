6951a8de1faf930b8baad1059218c7a8347541d2
Amos A <aanzele@users.noreply.github.com>
2020-11-17 13:57:15 -0500

    Add files via upload

---
+++ PS5.R
@@ -0,0 +1,32 @@
+library(sf)
+library(ggplot2)
+library(textdata)
+library(tmap)
+library(tmaptools)
+library(leaflet)
+library(dplyr)
+#library(rtweet)
+library(twitteR)
+library(dplyr)
+library(lubridate)
+library(tm)
+library(SentimentAnalysis)
+library(tidyr)
+library(tidytext)
+library(remotes)
+library(quanteda)
+library(maps)
+library(lubridate)
+library(syuzhet)
+library(SentimentAnalysis)
+
+get_sentiments("bing")
+get_sentiments("loughran")
+get_sentiments("afinn")
+get_sentiments("nrc")
+
+ps5 <- read.csv(file.choose())
+
+ps5Text<- ps5$text
+View(ps5Text)
+
